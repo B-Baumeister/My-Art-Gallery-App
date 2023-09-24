0ddbb0c8813689ed1bafe39b8d563414f1ef36bd
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-09-24 15:44:14 +0200

    add fetch URL and import SWR

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
 
     return config;
   },
+  reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import useSWR from "swr";
+//für normale RESTfull APIs with JSON data
+//wir benötigen eine fetcher function (ist wie wrapper der normalen fetch funktion)
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+//jetzt kann man die Funktion aufrufen als Componente
+function Character() {
+  const { data } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+
+  // render data
+  return <div>Hello {data.name}!</div>; // Hello Luke Skywalker!
+}
+
 export default function HomePage() {
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <Image src={data.imageSource} width={200} height={200} alt="hi" />
     </div>
   );
 }
